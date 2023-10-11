7e48c6a77c8d27d9fb75825062a4603e1bd515e4
manuvant <manutrovant@hotmail.com>
2023-10-11 19:24:29 -0300

    add TypeScript: The Basics

---
+++ helloWorld/hello.js
@@ -0,0 +1,12 @@
+// console.log('Hello world!');
+///////////
+// function greet(person, date) {
+//     console.log(`Hello ${person}, today is ${date}!`);
+// }
+// greet("Brendan");
+// Si ejecutamos tsc hello.tsnuevamente, ¡notará que obtenemos un error en la línea de comando!
+"use strict";
+// function greet(person, date) {
+//     console.log("Hello ".concat(person, ", today is ").concat(date.toDateString(), "!"));
+// }
+greet("Maddison", new Date());
